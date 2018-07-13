cd1871496a2d83a3188be0d5a9f0d66f78cb40e2
Paleari <DEPI@gft.com>
2018-07-13 13:06:42 +0200

    tdd + calc

---
+++ my_course/src/main.js
@@ -0,0 +1,7 @@
+const sum = (num1, num2) => num1 + num2;
+const sub = (num1, num2) => num1 - num2;
+const mult = (num1, num2) => num1 * num2;
+const div = (num1, num2) =>
+  num2 === 0 ? "nao é possível divisao por 0" : num1 / num2;
+
+export { sum, sub, mult, div };
